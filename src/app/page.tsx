2559dc28afc79d60f25c3b0e699481b719d86557
Fadak <ca3p3r.7@gmail.com>
2026-08-29 23:45:33 +0300

    update hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -66,7 +66,7 @@ export default async function Home() {
         <img
           src="/kilitbahir.jpg"
           alt="Kilitbahir"
-          className="absolute inset-0 w-full h-full object-cover opacity-70"
+          className="absolute inset-0 w-full h-full object-cover opacity-80"
         />
 
         {/* Overlay */}
